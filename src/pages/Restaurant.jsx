051bc9f0dae8c60967ac6b68e2ee16219911a6a8
bpeddapudi <82531209+bpeddapudi@users.noreply.github.com>
2023-02-06 22:06:37 -0800

    Fallback to queryparameter. JSON logic for each restaurant.

--- src/pages/Restaurant.jsx
+++ src/pages/Restaurant.jsx
@@ -1,8 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Footer from '../components/Footer/Footer';
 import Header from '../components/Header/Header';
 import Restaurants from '../data/restaurants.json';
-import MenuItems from '../data/menus/burger-king.json';
 import LinesEllipsis from 'react-lines-ellipsis'
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
@@ -17,13 +16,27 @@ import Alert from '@mui/material/Alert';
 
 const CURRENT_SUBDOMAIN = window.location.host.split('.')[0];
 const isLocalEnv = window.location.host.includes("localhost");
+
+/** Get Restaurant object from URL params */
 const getRestaurant = () => {
-  const filteredList = Restaurants.filter(resturant => resturant.slung_url === CURRENT_SUBDOMAIN);
+  /** Get restaurant name from queryparam */
+  const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+  });
+  if (params && params['restaurant']) {
+    const restaurantName = params['restaurant'];
+    const restaurantObj = Restaurants.filter(restaurant => restaurant.slung_url === restaurantName);
+    if (restaurantObj.length > 0)
+      return restaurantObj[0];
+  }
+
+  /** Get restaurant name from subdomain */
+  const filteredList = Restaurants.filter(restaurant => restaurant.slung_url === CURRENT_SUBDOMAIN);
   if (filteredList.length > 0)
     return filteredList[0];
   else if (isLocalEnv) {
-    const localEnvRestaurantList = Restaurants.filter(resturant => resturant.slung_url === 'burger-king-local');
-    return localEnvRestaurantList.length > 0 ? localEnvRestaurantList[0] :'';
+    const localEnvRestaurantList = Restaurants.filter(restaurant => restaurant.slung_url === 'burger-king-local');
+    return localEnvRestaurantList.length > 0 ? localEnvRestaurantList[0] : '';
   }
   else
     return '';
@@ -31,13 +44,22 @@ const getRestaurant = () => {
 
 function Restaurant(props) {
   const [restaurant, setRestaurant] = useState(getRestaurant());
+  const [menu, setMenu] = useState({});
   const [currentCategoryId, setCurrentCategoryId] = useState(0);
   const handleCategoryChange = (categoryIdIn) => {
     setCurrentCategoryId(categoryIdIn);
   }
 
-  const menu =
-    MenuItems.items.filter((el) => {
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/menus/${restaurant.menu_end_point}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setMenu(data);
+      });
+  }, [])
+
+  const RenderMenu = (menuItems) =>
+    menuItems.filter((el) => {
       return el.category_id === currentCategoryId + '' || currentCategoryId === 0
     })
       .map((item) => {
@@ -48,7 +70,7 @@ function Restaurant(props) {
                 <CardMedia
                   component="img"
                   height="240"
-                  image={item.images.length > 0 ? item.images[0] : ''}
+                  image={item.images.length > 0 ? item.images[0] : `${process.env.PUBLIC_URL}/images/skelitons/image-placeholder.png`}
                   alt="green iguana"
                 />
                 <CardContent>
@@ -80,12 +102,12 @@ function Restaurant(props) {
     <div>
       <Header restaurant={restaurant}></Header>
       {
-        restaurant === '' ?
+        restaurant === '' || menu.catergories === undefined || menu.items === undefined ?
           <Container maxWidth="lg"><Alert severity="error" sx={{ mt: 4 }}>Unable to find your restaurant with this subdomain:{CURRENT_SUBDOMAIN}</Alert></Container> :
           <Container maxWidth="lg">
-            <Categories categories={MenuItems.catergories} buttonClickHandler={handleCategoryChange} currentCategoryId={currentCategoryId}></Categories>
+            <Categories categories={menu.catergories} buttonClickHandler={handleCategoryChange} currentCategoryId={currentCategoryId}></Categories>
             <Grid container spacing={2} sx={{ mt: 1 }}>
-              {menu}
+              {RenderMenu(menu.items)}
             </Grid>
           </Container>
       }
